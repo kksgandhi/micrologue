4da84b4ee95d5d51ff6839c6e25cd285a16f8157
Kutub Gandhi <kksgandhi@gmail.com>
2021-06-22 17:11:00 -0400

    add option for typewriter effect

--- configuration.ts
+++ configuration.ts
@@ -2,3 +2,11 @@
 let startingPassageTitle = "intro";
 // Should unclicked links be cleared away?
 let clearOldLinks = true;
+
+// TYPEWRITER EFFECT
+// Enable typewriter effect
+let doTypewriterEffect = true;
+// Time between letters for a speaker, assuming you are using typewriter effect (milliseconds)
+let timeBetweenLetters = 30;
+// Time between speakers, assuming you are using typewriter effect (milliseconds)
+let timeBetweenSpeakers = 300;
--- main.ts
+++ main.ts
@@ -17,15 +17,10 @@ type passages = {
     [passageTitle: string]: passage
 }
 
-let renderPassage = (passage: passage) => {
-    let main = document.getElementById("main")!;
-    passage.utterances.map(utterance => {
-        let utteranceElem = document.createElement("p");
-        utteranceElem.setAttribute("class", utterance.speaker);
-        utteranceElem.innerText = utterance.text;
-        main.appendChild(utteranceElem);
-    });
-    passage.links.forEach(link => {
+type renderer = (passage: passage) => void;
+
+let renderLinksGeneric = (main: Element, links: link[], renderer: renderer) => {
+    links.forEach(link => {
         let linkElem = document.createElement("a");
         linkElem.innerText = link.text;
         linkElem.onclick = () => {
@@ -38,7 +33,7 @@ let renderPassage = (passage: passage) => {
                                        if (clearOldLinks) elem.remove();
                                        else elem.setAttribute("class", "unclicked-no-clear")
                                    });
-                renderPassage(passages[link.passageTitle]);
+                renderer(passages[link.passageTitle]);
                 window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
                 return false;
             }
@@ -50,4 +45,49 @@ let renderPassage = (passage: passage) => {
     });
 }
 
-renderPassage(passages[startingPassageTitle]);
+let renderPassageSimple = (passage: passage) => {
+    let main = document.getElementById("main")!;
+    passage.utterances.forEach(utterance => {
+        let utteranceElem = document.createElement("p");
+        utteranceElem.setAttribute("class", utterance.speaker);
+        utteranceElem.innerText = utterance.text;
+        main.appendChild(utteranceElem);
+    });
+    renderLinksGeneric(main, passage.links, renderPassageSimple);
+}
+
+let sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+let renderPassageTypewriter = async (passage: passage) => {
+    let main = document.getElementById("main")!;
+    for (let idx in passage.utterances) {
+        let utterance = passage.utterances[idx];
+        let utteranceElem = document.createElement("p");
+        utteranceElem.setAttribute("class", utterance.speaker);
+        main.appendChild(utteranceElem);
+        let characters = Array.from(utterance.text);
+        for (let charidx in characters) {
+            let character = characters[charidx];
+            if (character === " ") character = "&nbsp;"
+            utteranceElem.innerHTML = utteranceElem.innerHTML + character;
+            await sleep(timeBetweenLetters);
+        }
+        await sleep(timeBetweenSpeakers);
+    }
+    /**
+    passage.utterances.forEach(async (utterance) => {
+        let utteranceElem = document.createElement("p");
+        utteranceElem.setAttribute("class", utterance.speaker);
+        main.appendChild(utteranceElem);
+        utterance.text.split('').forEach(async (character) => {
+            utteranceElem.innerText = utteranceElem.innerText + character;
+            await sleep(5);
+        });
+    });
+    */
+    renderLinksGeneric(main, passage.links, renderPassageTypewriter);
+}
+
+if (doTypewriterEffect)
+    renderPassageTypewriter(passages[startingPassageTitle]);
+else
+    renderPassageSimple(passages[startingPassageTitle]);
